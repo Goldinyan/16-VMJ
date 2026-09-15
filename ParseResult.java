f61cc9f0f11e6cc5b8c27f0e93898cb283c9d9b1
Ansgar Seifert <seifert.ansgar@gmail.com>
2026-09-15 13:09:15 +0200

    Introduce generic ParseReturn class
    Added a generic class ParseReturn to hold ParseResult and a value.

--- ParseResult.java
+++ ParseResult.java
@@ -6,3 +6,25 @@ public enum ParseResult {
     SYMBOL_NOT_FOUND,  // like if a label doesnt exist in symbol table or sum 
     GENERIC_ERROR     
 }
+
+/*
+int regId1 = getRegId(parts[1]);
+int regId2 = getRegId(parts[2]);
+                
+if (regId1 == -1 || regId2 == -1) {
+    return null;
+}
+
+ParseReturn regID1Result = getRegId(parts[i]);
+int regID1 = regID1Result.getResult()
+// soll irgendwie unpacken ob es value gibt oder nicht
+*/
+
+public class <T> ParseReturn {
+    public ParseResult result;
+    public T value;
+
+    public
+        
+
+}
