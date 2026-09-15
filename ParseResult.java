b0a21c54ef871cd8c178cf1280c6255e57d55d66
Ansgar Seifert <seifert.ansgar@gmail.com>
2026-09-15 12:50:06 +0200

    Created ParseResult for better Func return types

---
+++ ParseResult.java
@@ -0,0 +1,8 @@
+public enum ParseResult {
+    SUCCESS,
+    SYNTAX_ERROR,      // Wrong format or missing statements, etc 
+    INVALID_REGISTER,  // register id not within 0 to 15 
+    INVALID_IMMEDIATE, // Number to big to fit into byte or short 
+    SYMBOL_NOT_FOUND,  // like if a label doesnt exist in symbol table or sum 
+    GENERIC_ERROR     
+}
